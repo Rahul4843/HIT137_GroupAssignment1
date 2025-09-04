b41c1da0a68990abf781145ef8fdd603d549f12b
Abink48 <achalise@students.federation.edu.au>
2025-09-04 20:28:18 +0930

    Question-2: Function 2: Largest Temperature Range

--- Assignment2/Question2.py
+++ Assignment2/Question2.py
@@ -6,7 +6,7 @@ def readCsvFiles(folder="temperatures"):
     allData = []
     for file in os.listdir(folder):
         if file.endswith(".csv"):
-            df = pd.read_csv(os.path.join(folder, file)) #Reading each CSV file into a pandas data frame
+            df = pd.read_csv(os.path.join(folder, file)) #Reading each CSV file into a pandas data
             allData.append(df)
     return allData
 
@@ -41,8 +41,42 @@ def seasonsAverage():
                 f.write(f"{season}: No data\n")
 
     print("Seasonal averages saved to average_temp.txt")
+     
+# Find the station having largest temperature range
+def tempeRange():
+    StationRange = {}  # Place to store temperature ranges for each station
+
+    # Loop through all CSV files and read the tmperature data
+    for df in readCsvFiles():
+        for _, row in df.iterrows():
+            station = row["STATION_NAME"]  
+            temps = row.loc["January":"December"].dropna()  # Get the temperature data for the year
+
+            if station not in StationRange:  
+                StationRange[station] = {"max": temps.max(), "min": temps.min()}
+            else:
+                # Update the max and min temperature for the station
+                StationRange[station]["max"] = max(StationRange[station]["max"], temps.max())
+                StationRange[station]["min"] = min(StationRange[station]["min"], temps.min())
+
+    # Calculate the temperature range  for each station
+    for station, range_values in StationRange.items():
+        range_values["range"] = range_values["max"] - range_values["min"]
+
+    # Find the station with the larest temperature range
+    maxRange = max(range_values["range"] for range_values in StationRange.values())  
+    LargestStations = [station for station, range_values in StationRange.items() if range_values["range"] == maxRange]  
+
+    # Write results to a file
+    with open("largest_temp_range_station.txt", "w") as f:
+        for station in LargestStations:
+            range_values = StationRange[station]
+            f.write(f"{station}: Range {round(range_values['range'],2)}°C "
+                    f"(Max: {round(range_values['max'],2)}°C, Min: {round(range_values['min'],2)}°C)\n")
+
+    print("Temperature range saved to largest_temp_range_station.txt") 
 
 
-  
-# Calling functions
+# Calling the functions
 seasonsAverage()
+tempeRange()
