10ab83b751d89e00d535a197f8e14c9d29a81819
Sandeep <sandeepprasai99@gmail.com>
2025-10-03 19:27:39 +1000

    Adding main class

---
+++ Assignment3/main.py
@@ -0,0 +1,10 @@
+# main.py
+"""
+Main python file which starts the program.
+To run, use python3 main.py
+It will open up the GUI window using Tkinter.
+"""
+from gui import runGui
+
+if _name_ == "_main_":
+    runGui()
